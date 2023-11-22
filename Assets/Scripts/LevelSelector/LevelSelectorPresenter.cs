77f833dd1540b3150d91e3b50ae0c5e229ce32d6
stijn7621 <stijn.mulder10@gmail.com>
2023-11-22 15:12:41 +0100

    added basic implementation for level select

---
+++ Assets/Scripts/LevelSelector/LevelSelectorPresenter.cs
@@ -0,0 +1,57 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Assets.Scripts.LevelSelector
+{
+    public class LevelSelectorPresenter : MonoBehaviour
+    {
+        [field: SerializeField]
+        public int LevelCount { get; set; } = 2;
+
+        private const int PageCount = 3;
+
+        private PlayLevelButton[] playLevelButtons;
+
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+            // Construct level buttons
+            playLevelButtons = new PlayLevelButton[PageCount];
+            for (int i = 0; i < PageCount; i++)
+            {
+                playLevelButtons[i] = new PlayLevelButton(root.Q<TemplateContainer>($"PlayButton{i + 1}"));
+            }
+            
+            // Initialize level buttons
+            SetLevelPage(1);
+
+            // todo Add functionality for navigation buttons
+            root.Q<VisualElement>("PreviousPageButton").visible = false;
+            root.Q<VisualElement>("NextPageButton").visible = false;
+        }
+
+        private void SetLevelPage(int page)
+        {
+            int x = (page - 1) * PageCount;
+            for (int i = 0; i < PageCount; i++)
+            {
+                //todo
+                int currentLevel = ++x;
+                bool visible = currentLevel <= LevelCount;
+
+                playLevelButtons[i].SetLevel(currentLevel, visible);
+            }
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public static class SceneLoader
+    {
+        public static void LoadLevel(int level = 1)
+        {
+            //SceneManager.LoadScene($"Level_{level}", LoadSceneMode.Single);
+        }
+    }
+}
