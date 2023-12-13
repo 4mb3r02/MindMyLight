f36f15662e511f4961cc757b13be5914b61391a5
stijn7621 <stijn.mulder10@gmail.com>
2023-12-13 11:18:59 +0100

    added spawn areas

--- Assets/Scripts/Level 2/LevelGenerator.cs
+++ Assets/Scripts/Level 2/LevelGenerator.cs
@@ -1,39 +1,89 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Runtime.InteropServices;
 using Assets.Scripts.General;
-using Unity.VisualScripting;
+using Assets.Scripts.Level_2.Collisions;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    [field: SerializeField]
-    public GameObject CloudPrefab { get; set; }
+    [Header("----------- Level Settings -----------")]
+    public int AmountOfBalloons = 4;
+    public GameObject BalloonPrefab;
+    public GameObject LevelLayer;
+    public RectTransform[] SpawnAreas;
 
-    [field: SerializeField] 
-    public GameObject BackgroundLayer { get; set; }
-
-    [field: SerializeField] 
-    public int DistanceBetweenRadius { get; set; } = 50;
+    [Header("----------- Background -----------")]
+    public GameObject CloudPrefab;
+    public GameObject CloudLayer;
+    public int DistanceBetweenRadius = 50;
 
     // Start is called before the first frame update
     void Start()
     {
-        var rectSize = BackgroundLayer.GetComponent<RectTransform>().rect.size;
-
-        var points = PoissonDiscSampling.GeneratePoints(DistanceBetweenRadius, rectSize);
-        foreach (var point in points)
-        {
-            var position = BackgroundLayer.transform.TransformPoint(point);
-            Instantiate(CloudPrefab, position, Quaternion.identity, BackgroundLayer.transform);
-        }
+        CreateClouds();
+        //todo
+        ConstructLevel();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    void ConstructLevel()
+    {
+        CreateBalloons();
+    }
+
+    void CreateBalloons()
+    {
+        // var worldCorners = GetWorldCorners();
+        //   var bl = worldCorners[0];
+        //var tr = worldCorners[2];
+
+        // var settings = PoissonDiskSampling.GetSettings(bl, tr, DistanceBetweenRadius);
+
+
+        //var settings = PoissonDiskSampling.GetSettings(bl, tr, DistanceBetweenRadius, AmountOfBalloons);
+
+        // var points = PoissonDiskSampling.Sampling(settings);
+        var balloon = new Balloon()
+        {
+            BalloonPrefab = BalloonPrefab,
+            LevelLayer = LevelLayer
+        };
+        // LevelLayer.GetComponent<RectTransform>();
+        for (int i = 0; i < AmountOfBalloons; i++)
+        {
+            int area = Random.Range(0, SpawnAreas.Length);
+            var spawnArea = SpawnAreas[area];
+            float x = Random.Range(spawnArea.rect.xMin, spawnArea.rect.xMax);
+            float y = Random.Range(spawnArea.rect.yMin, spawnArea.rect.yMax);
+            Vector2 spawnPoint = new Vector2(x, y);
+            var point= spawnArea.TransformPoint(spawnPoint);
+            balloon.Spawn(point);
+        }
+    }
+
+    void CreateClouds()
+    {
+        var worldCorners = GetWorldCorners();
+        var bl = worldCorners[0];
+        var tr = worldCorners[2];
+
+        var settings = PoissonDiskSampling.GetSettings(bl, tr, DistanceBetweenRadius);
+        settings.IterationPerPoint = PoissonDiskSampling.CalculateIterationPerPoint(settings);
+
+        var points = PoissonDiskSampling.Sampling(settings);
+        foreach (var point in points)
+        {
+            Instantiate(CloudPrefab, point, Quaternion.identity, CloudLayer.transform);
+        }
+    }
+
+    Vector3[] GetWorldCorners()
+    {
+        var transform = GetComponent<RectTransform>();
+        Vector3[] v = new Vector3[4];
+        transform.GetWorldCorners(v);
+        return v;
     }
 }
