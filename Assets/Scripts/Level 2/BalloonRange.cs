96fc82b0f572571ad8ef7804006a73023e329a5e
Fineshoul <70943826+Fineshoul@users.noreply.github.com>
2023-12-15 10:44:23 +0100

    Name changes

--- Assets/Scripts/Level 2/BalloonRange.cs
+++ Assets/Scripts/Level 2/BalloonRange.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class BalloonMovement : MonoBehaviour, ICollectibleMovement
+public class BalloonRange : MonoBehaviour, ICollectibleRange
 {
     float moveAwaySpeed = 3;
     bool exitRange = true;
