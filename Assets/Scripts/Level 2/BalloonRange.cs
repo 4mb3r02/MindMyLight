ae6aeb4ec365a07dc5129f76cf6e3e275f0fb8b9
Fineshoul <70943826+Fineshoul@users.noreply.github.com>
2023-12-15 10:50:15 +0100

    Interface deleted

--- Assets/Scripts/Level 2/BalloonRange.cs
+++ Assets/Scripts/Level 2/BalloonRange.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class BalloonRange : MonoBehaviour, ICollectibleRange
+public class BalloonRange : MonoBehaviour
 {
     float moveAwaySpeed = 3;
     bool exitRange = true;
